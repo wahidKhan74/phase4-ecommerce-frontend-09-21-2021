12586fad075af5cbfc89e3723463e60451cb8e39
Waheed Khan <wahid.khan74@gmail.com>
2021-09-28 06:50:43 +0000

    adding http  request calls

--- src/app/products/product-create/product-create.component.ts
+++ src/app/products/product-create/product-create.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
+import { ApiService } from 'src/app/service/api.service';
 
 @Component({
   selector: 'app-product-create',
@@ -7,7 +9,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductCreateComponent implements OnInit {
 
-  constructor() { }
+  public productForm: FormGroup;
+  public submitted:boolean = false;
+  public response :string="";
+
+  constructor(private formBuilder: FormBuilder, private apiService: ApiService) {
+    // init productForm
+    this.productForm = this.formBuilder.group({
+        name: ["",[Validators.required, Validators.minLength(3), Validators.maxLength(15)]],
+        price:["",[Validators.required]],
+        description:["",Validators.required]
+    });
+
+   }
+
+   public onSubmit(form:any) {
+    if(form.valid){
+      this.submitted = true;
+      // console.log(this.productForm.value);
+      this.apiService.addProduct(this.productForm.value).subscribe(res=> {
+        this.response ="Product is created Succcessfully !";
+      }, err=> {
+        this.response ="Product creation failed !";
+      })
+    } else{
+      console.log("Invalid Form");
+      this.validateForm(form);
+    }
+  }
+
+  public validateForm(form: any) {
+    Object.keys(form.controls).forEach(field=>{
+      const control = form.get(field);
+      if(control instanceof FormControl) {
+        control.markAsTouched({ onlySelf : true });
+      } else{
+        this.validateForm(control);
+      }
+    });
+  }
+
+  hasError(field:any){
+    return (this.productForm.get(field)?.invalid && this.productForm.get(field)?.touched && this.productForm.get(field)?.errors );
+  }
+
+  //get methods for form
+  get f() {
+    return this.productForm.controls;
+  }
 
   ngOnInit(): void {
   }
--- src/app/products/product-list/product-list.component.ts
+++ src/app/products/product-list/product-list.component.ts
@@ -9,16 +9,28 @@ import { ApiService } from 'src/app/service/api.service';
 export class ProductListComponent implements OnInit {
 
   constructor(private apiService : ApiService) { }
+  products:any;
 
   ngOnInit(): void {
+    this.getAllProduct();
+  }
+
+  onDelete(id:string) {
+    this.apiService.deltetProduct(id).subscribe(res=>{
+      console.log("product is deleted !");
+      this.getAllProduct();
+    })
+  }
+
+  getAllProduct() {
     this.apiService.getProduct().subscribe(
       (res)=>{
-        console.log(res);
+       // console.log(res);
+        this.products = res;
       },
       (error)=>{
         console.log(error);
       }
     )
   }
-
 }
